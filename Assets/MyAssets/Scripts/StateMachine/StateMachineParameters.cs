563f6f39fc222bc5d01119956214683a63feb8dc
Dan C <djc5627@gmail.com>
2020-08-25 14:48:35 -0400

    Add log message for parameters not found

--- Assets/MyAssets/Scripts/StateMachine/StateMachineParameters.cs
+++ Assets/MyAssets/Scripts/StateMachine/StateMachineParameters.cs
@@ -16,33 +16,33 @@ public class StateMachineParameters : SerializedScriptableObject
     public void SetBool(string paramName, bool value)
     {
         if (BoolParameters.ContainsKey(paramName))
-        {
             BoolParameters[paramName] = value;
-        }
+        else
+            Debug.Log($"Can't find Bool parameter {paramName}!");
     }
     
     public void SetTrigger(string paramName)
     {
         if (TriggerParameters.ContainsKey(paramName))
-        {
             TriggerParameters[paramName] = true;
-        }
+        else
+            Debug.Log($"Can't find Trigger parameter {paramName}!");
     }
     
     public void SetFloat(string paramName, float value)
     {
         if (FloatParameters.ContainsKey(paramName))
-        {
             FloatParameters[paramName] = value;
-        }
+        else
+            Debug.Log($"Can't find Float parameter {paramName}!");
     }
     
     public void SetInt(string paramName, int value)
     {
         if (IntParameters.ContainsKey(paramName))
-        {
             IntParameters[paramName] = value;
-        }
+        else
+            Debug.Log($"Can't find Int parameter {paramName}!");
     }
     
 }

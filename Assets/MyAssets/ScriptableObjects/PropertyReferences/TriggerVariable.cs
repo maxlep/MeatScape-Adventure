127a6c43d5d5b107cf73410af9a8a6fe9fa5c522
maxlep <mlepkows@stevens.edu>
2020-08-27 00:35:41 -0400

    Add trigger variable scriptable object.

---
+++ Assets/MyAssets/ScriptableObjects/PropertyReferences/TriggerVariable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[InlineEditor(InlineEditorObjectFieldModes.Foldout)]
+[CreateAssetMenu(fileName = "TriggerVariable", menuName = "Variables/TriggerVariable", order = 0)]
+public class TriggerVariable : ScriptableObject
+{
+    [TextArea] [HideInInlineEditors] public String Description;
+    
+    public delegate void OnUpdate_();
+    public event OnUpdate_ OnUpdate;
+
+    public void Activate()
+    {
+        Debug.Log("ACTIVATED");
+        OnUpdate?.Invoke();
+    }
+}

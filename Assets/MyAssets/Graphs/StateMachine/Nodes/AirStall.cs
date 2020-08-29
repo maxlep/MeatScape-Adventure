c1b45c0405798adf591a72732bdb42b51e72c8ba
Dan C <djc5627@gmail.com>
2020-08-28 23:03:27 -0400

    Add airStall class

---
+++ Assets/MyAssets/Graphs/StateMachine/Nodes/AirStall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class AirStall : PlayerStateNode
+{
+    [FoldoutGroup("")] [LabelWidth(120)] [SerializeField] private bool restrictX = true;
+    [FoldoutGroup("")] [LabelWidth(120)] [SerializeField] private bool restrictY = true;
+    [FoldoutGroup("")] [LabelWidth(120)] [SerializeField] private bool restrictZ = true;
+    [FoldoutGroup("")] [LabelWidth(120)] [SerializeField] private Vector3Reference NewVelocityOut;
+
+
+    public override void Enter()
+    {
+        base.Enter();
+        playerController.onStartUpdateVelocity += UpdateVelocity;
+    }
+    
+    private void UpdateVelocity(Vector3 currentVelocity)
+    {
+        if (restrictX) currentVelocity.x = 0f;
+        if (restrictY) currentVelocity.y = 0f;
+        if (restrictZ) currentVelocity.z = 0f;
+
+        NewVelocityOut.Value = currentVelocity;
+    }
+}

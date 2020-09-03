d4271633a24b7bd5ef0759f18f6b8ae9fbdd1f13
Dan C <djc5627@gmail.com>
2020-09-03 16:59:04 -0400

    Add time manager and implement basic pause functionality

---
+++ Assets/MyAssets/Scripts/Managers/TimeManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AmplifyShaderEditor;
+using MyAssets.ScriptableObjects.Variables;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager Instance;
+
+    private bool isPaused = false;
+    private float beforeTimeScale;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(this);
+
+        InputManager.Instance.onPauseGame += TogglePauseGame;
+    }
+
+    public void TogglePauseGame()
+    {
+        if (!isPaused)
+        {
+            beforeTimeScale = Time.timeScale;
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+        else
+        {
+            Time.timeScale = beforeTimeScale;
+            isPaused = false;
+        }
+    }
+}

6080c25bf16f96ea27bb6d98aa7d5304d98bc9b5
Dan C <djc5627@gmail.com>
2020-09-08 12:03:07 -0400

    Add breakpoint node

---
+++ Assets/MyAssets/Graphs/StateMachine/Nodes/BreakpointNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MyAssets.ScriptableObjects.Variables;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class BreakpointNode : PlayerStateNode
+{
+    [HideIf("$zoom")] [LabelWidth(120)] [TextArea] [SerializeField] private string Message = "";
+
+
+    public override void Enter()
+    {
+        base.Enter();
+        Debug.LogError(Message);
+    }
+}
---
+++ Assets/MyAssets/Graphs/StateMachine/Nodes/Editor/BreakpointStateNodeEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNodeEditor;
+
+[NodeEditor.CustomNodeEditorAttribute(typeof(BreakpointNode))]
+public class BreakpointNodeEditor : NodeEditor
+{
+    public override Color GetTint()
+    {
+        Color tint;
+        bool isActiveNode = (target as BreakpointNode).isActiveState;
+
+        if (isActiveNode)
+            tint = new Color(110f/255f, 110f/255f, 60f/255f, 1f);
+        else
+            tint = new Color(120f/255f, 30f/255f, 40f/255f, 1f);
+        
+        return tint;
+    }
+}

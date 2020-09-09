ae252d89df9b7ef8389321c33c96f5a8760abe35
Dan C <djc5627@gmail.com>
2020-09-09 18:47:02 -0400

    Add interface forgot to push

---
+++ Assets/MyAssets/Graphs/StateMachine/Nodes/TransitionConditions/ITransitionCondition.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MyAssets.ScriptableObjects.Variables;
+using UnityEngine;
+
+public interface ITransitionCondition
+{
+    void Init(string transitionName);
+    bool Evaluate(TriggerVariable receivedTrigger);
+}

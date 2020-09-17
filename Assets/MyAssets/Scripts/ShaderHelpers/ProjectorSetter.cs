cc61ad2c7a04ea8d258c151729fcdbae6846f48e
maxlep <mlepkows@stevens.edu>
2020-09-17 12:24:20 -0400

    Add player blob shadow

---
+++ Assets/MyAssets/Scripts/ShaderHelpers/ProjectorSetter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MyAssets.ScriptableObjects.Variables;
+using UnityEngine;
+
+namespace MyAssets.Scripts.ShaderHelpers
+{
+    public class ProjectorSetter : MonoBehaviour
+    {
+        [SerializeField] private Projector projector;
+        [SerializeField] private FloatVariable variable;
+
+        private void Awake()
+        {
+            UpdateProjector();
+            variable.Subscribe(UpdateProjector);
+        }
+
+        public void UpdateProjector()
+        {
+            projector.orthographicSize = variable.Value;
+            Debug.Log($"Set orthographic size {variable.Value} {projector.orthographicSize}");
+        }
+    }
+}

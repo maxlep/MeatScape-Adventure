ff395e817f1eabd10fa561fc8857c54ff599b64e
Dan C <djc5627@gmail.com>
2020-10-18 15:14:09 -0400

    Add usePhysics option to rotate over time. Apply to meatclump for smooth rotation"

--- Assets/MyAssets/Scripts/Misc/RotateOverTime.cs
+++ Assets/MyAssets/Scripts/Misc/RotateOverTime.cs
@@ -6,19 +6,49 @@ using UnityEngine;
 public class RotateOverTime : MonoBehaviour
 {
     [SerializeField] private bool UseGlobalAxis = true;
+    [SerializeField] private bool UsePhysics = false;
     [SerializeField] private Vector3 RotationAxis = Vector3.up;
     [SerializeField] private float RotSpeed = 10;
 
     private bool enableRotation = true;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        if (UsePhysics) rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
-        if (!enableRotation) return;
-        
+        if (!enableRotation || UsePhysics) return;
+
+        //Rotate with Transform
         if (UseGlobalAxis)
+        {
             transform.Rotate(RotationAxis.normalized, RotSpeed * Time.deltaTime, Space.World);
+        }
         else
+        {
             transform.Rotate(RotationAxis.normalized, RotSpeed * Time.deltaTime, Space.Self);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!enableRotation || !UsePhysics) return;
+
+        //Rotate with Physics
+        if (UseGlobalAxis)
+        {
+            Vector3 localAxis = transform.InverseTransformDirection(RotationAxis.normalized);
+            Quaternion rot = Quaternion.AngleAxis(RotSpeed * Time.deltaTime, localAxis.normalized);
+            rb.MoveRotation(rb.rotation * rot);
+        }
+        else
+        {
+            Quaternion rot = Quaternion.AngleAxis(RotSpeed * Time.deltaTime, RotationAxis.normalized);
+            rb.MoveRotation(rb.rotation * rot);
+        }
     }
 
     public void StartRotating()

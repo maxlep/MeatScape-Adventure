933f5c1eb5e5bea8a189e7c1d217c12a15cba98a
Dan C <djc5627@gmail.com>
2020-11-03 20:56:29 -0500

    Several enhancements to enemy spawners.

---
+++ Assets/MyAssets/ScriptableObjects/EnemySpawner/EnemyTypeContainer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[Required] [InlineEditor]
+[CreateAssetMenu(fileName = "EnemyTypeContainer", menuName = "EnemyTypeContainer", order = 0)]
+public class EnemyTypeContainer : ScriptableObject
+{
+    [AssetsOnly] [ListDrawerSettings(Expanded = true)] public List<GameObject> EnemyPrefabs;
+}
--- Assets/MyAssets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/MyAssets/Scripts/Enemy/EnemySpawner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using AmplifyShaderEditor;
 using BehaviorDesigner.Runtime.Tasks.Unity.UnityCharacterController;
 using MyAssets.ScriptableObjects.Variables;
@@ -7,99 +8,212 @@ using Shapes;
 using UnityEngine;
 using Sirenix.OdinInspector;
 using Sirenix.Utilities;
+using UnityEditor;
 using UnityEngine.Jobs;
 using UnityEngine.Rendering;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject enemyPrefab;
-    [SuffixLabel("seconds", Overlay = true)] [SerializeField] private float spawnTime = 300;
+    [SuffixLabel("seconds", Overlay = true)] [SerializeField] 
+    private float SpawnTime = 1;
     
-    [Required("Null or missing patrol points! Will default to just spawn point.")]
-    [SerializeField] private List<Transform> patrolPoints = new List<Transform>();
+    [SerializeField] [ValueDropdown("GetEnemyDropdown")] [OnValueChanged("StoreEnemyPrefabMeshes")]
+    private GameObject EnemyPrefab;
+    
+    [SerializeField] [PropertySpace(5f, 5f)]
+    private EnemyTypeContainer EnemyTypes;
+    
+    [SerializeField] [PropertySpace(5f, 5f)]
+    private GameObject PatrolPointPrefab;
 
-    private void Awake()
+    [InfoBox("Patrol Points should only be added via buttons. Removing patrol point deletes its GameObject. " +
+             "Patrol Points require \"PatrolPoint\" tag (Add button does this for you).")]
+    [Required("Null or missing patrol points!")]
+    [SerializeField] [ListDrawerSettings(Expanded = true, CustomRemoveElementFunction = "RemovePatrolPoint")] 
+    private List<Transform> PatrolPoints = new List<Transform>();
+    
+    private List<(Transform transform, Mesh mesh)> enemyTransformMeshTuples = new List<(Transform transform, Mesh mesh)>();
+    private string patrolPointTag = "PatrolPoint";
+    private string patrolPointPrefix = "Point";
+
+    #region Inspector/Odin Methods
+
+    private List<GameObject> GetEnemyDropdown()
     {
-        ValidatePatrolPoints();
-        SpawnEnemy();
+        return EnemyTypes.EnemyPrefabs;
     }
     
     private void ValidatePatrolPoints()
     {
-        if (patrolPoints.IsNullOrEmpty())
+        //Remove points that are null or dont have correct tag
+        for (int i = PatrolPoints.Count - 1; i > 0; i--)
         {
-            patrolPoints = new List<Transform>();
-            patrolPoints.Add(transform);
-            return;
-        }
-
-        foreach (var patrolPoint in patrolPoints)
-        {
-            if (patrolPoint == null)
+            if (PatrolPoints[i] == null || PatrolPoints[i].tag != patrolPointTag)
             {
-                patrolPoints = new List<Transform>();
-                patrolPoints.Add(transform);
-                return;
+                PatrolPoints.RemoveAt(i);
             }
         }
     }
 
+    [Button(ButtonSizes.Medium)]
+    private void AddPatrolPoint()
+    {
+        //Spawn at last point or this transform
+        Vector3 newPointPosition = (PatrolPoints.Count > 0) ?
+            PatrolPoints[PatrolPoints.Count - 1].position : transform.position;
+
+        var newPoint = GameObject.Instantiate(PatrolPointPrefab, newPointPosition, 
+            Quaternion.identity, transform);
+
+        newPoint.tag = patrolPointTag;
+        newPoint.name = $"{patrolPointPrefix}{PatrolPoints.Count + 1}";
+        PatrolPoints.Add(newPoint.transform);
+    }
+    
+    [Button(ButtonSizes.Medium)]
+    private void RenamePatrolPoints()
+    {
+        for (int i = 0; i < PatrolPoints.Count; i++)
+        {
+            PatrolPoints[i].name = $"{patrolPointPrefix}{i + 1}";
+        }
+    }
+
+    private void RemovePatrolPoint(Transform pointTransform)
+    {
+        //DestroyImmediate(pointTransform.gameObject);
+        Undo.DestroyObjectImmediate(pointTransform.gameObject);
+    }
+
+    #endregion
+    
+    
+    private void Awake()
+    {
+        SpawnEnemy();
+    }
+
     private void SpawnEnemy() {
-        GameObject enemy = Instantiate(enemyPrefab, transform.position, transform.rotation, transform);
+        GameObject enemy = Instantiate(EnemyPrefab, transform.position, transform.rotation, transform);
         EnemyController enemyScript = enemy.GetComponent<EnemyController>();
         
-        enemyScript.Initialize(patrolPoints);
+        enemyScript.Initialize(PatrolPoints);
         enemyScript.OnDeath += () => {
-            LeanTween.value(0f, 1f, spawnTime).setOnComplete(_ => SpawnEnemy());
+            LeanTween.value(0f, 1f, SpawnTime).setOnComplete(_ => SpawnEnemy());
         };
     }
+    
+    private void OnValidate()
+    {
+        if (enemyTransformMeshTuples.IsNullOrEmpty())
+        {
+            StoreEnemyPrefabMeshes();
+        }
+
+        ValidatePatrolPoints();
+    }
+
+    #region Gizmos
 
     private void OnDrawGizmos()
     {
         Draw.LineGeometry = LineGeometry.Volumetric3D;
-        Draw.LineThicknessSpace = ThicknessSpace.Pixels;
-        Draw.LineThickness = 3f;
+        Draw.LineThicknessSpace = ThicknessSpace.Meters;
+        Draw.LineThickness = .2f;
         Draw.LineDashStyle = DashStyle.DefaultDashStyleLine;
 
-        //Draw spawn cuboid
-        Draw.Cuboid(ShapesBlendMode.Transparent, ThicknessSpace.Meters, transform.position, Quaternion.identity, 
-            Vector3.one * 1.5f, new Color(209f/255f, 130f/255f, 171f/255f, .5f));
+        //Draw spawn sphere
+        // Draw.Cuboid(ShapesBlendMode.Transparent, ThicknessSpace.Meters, transform.position, Quaternion.identity, 
+        //     Vector3.one * 1.5f, new Color(209f/255f, 130f/255f, 171f/255f, .5f));
 
-        //Draw patrol paths
-        for (int i = 0; i < patrolPoints.Count; i++)
+        DrawPatrolPathGizmos();
+        DrawEnemyMeshGizmo();
+    }
+
+    private void DrawPatrolPathGizmos()
+    {
+        //Draw dotted line from spawn to first patrol point
+        Draw.LineDashed(transform.position, PatrolPoints[0].position,
+            new Color(150f/255f, 50f/255f, 220f/255f, .5f));
+        
+        
+        //Draw dotted lines with arrows (cones) between patrol points
+        for (int i = 0; i < PatrolPoints.Count; i++)
         {
-            Vector3 startPoint = patrolPoints[i].position;
+            Vector3 startPoint = PatrolPoints[i].position;
             Vector3 endPoint;
             Vector3 forward;
             Vector3 coneOffset;
-            float coneRadius = .75f;
-            float coneHeight = 2f;
+            float coneRadius = .5f;
+            float coneHeight = 1.35f;
             float startSphereRadius = 1f;
             Color lineColor = new Color(235f/255f, 201f/255f, 12f/255f, .5f);
+            Color coneColor = new Color(235f/255f, 100f/255f, 0f/255f, .5f);
             Color startSphereColor = new Color(181f/255f, 14f/255f, 21f/255f, .5f);
 
-            if (i == 0)
+            if (i == 0 && PatrolPoints.Count > 1)
             {
                 Draw.Sphere(startPoint, startSphereRadius, startSphereColor);
             }
 
-            if (i < patrolPoints.Count - 1)
+            if (i < PatrolPoints.Count - 1)
             {
-                endPoint = patrolPoints[i + 1].position;
+                endPoint = PatrolPoints[i + 1].position;
             }
             else
             {
-                endPoint = patrolPoints[0].position;
+                endPoint = PatrolPoints[0].position;
             }
             
             forward = (endPoint- startPoint).normalized;
-            coneOffset = -forward * coneHeight;
+            coneOffset = -forward * (coneHeight * 1.15f);
             Draw.LineDashed(startPoint, endPoint, lineColor);
             Quaternion coneRot = (!Mathf.Approximately(forward.magnitude, 0f))
                 ? Quaternion.LookRotation(forward)
                 : Quaternion.identity;
-            Draw.Cone(endPoint + coneOffset, coneRot, coneRadius,
-                coneHeight, lineColor);
+            Draw.Cone(endPoint + coneOffset, coneRot, coneRadius, coneHeight, coneColor);
         }
     }
+
+    private void DrawEnemyMeshGizmo()
+    {
+        foreach (var (meshTransform, mesh) in enemyTransformMeshTuples)
+        {
+            Vector3 position = meshTransform.position + transform.position;
+            Quaternion rotation = meshTransform.rotation;
+            Vector3 scale = meshTransform.lossyScale;
+            Matrix4x4 matrix = Matrix4x4.TRS(position, rotation, scale);
+            
+            
+            if (!Application.isPlaying) 
+                Gizmos.color = new Color(.5f, .5f, .5f, .5f);
+            else
+                Gizmos.color = new Color(.5f, .5f, .5f, .25f);
+            
+            
+            Gizmos.DrawMesh(mesh, position, rotation, scale);
+            
+            
+                
+        }
+    }
+
+    private void StoreEnemyPrefabMeshes()
+    {
+        if (EnemyPrefab == null) return;
+        
+        enemyTransformMeshTuples.Clear();
+
+        MeshFilter[] meshFilters = EnemyPrefab.GetComponentsInChildren<MeshFilter>();
+
+        foreach (var meshFilter in meshFilters)
+        {
+            enemyTransformMeshTuples.Add((meshFilter.transform, meshFilter.sharedMesh));
+        }
+    }
+    
+
+    #endregion
+
+    
 }

6193dd7a6d6e6bbd5779843ea1347ccf47c8b812
Dan C <djc5627@gmail.com>
2021-01-15 22:41:56 -0500

    Suffering blob joins the meatscape. AI tick rate controller. Graph switcher.

---
+++ Assets/MyAssets/Scripts/AI/GraphSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using Pathfinding;
+using UnityEngine;
+
+public class GraphSwitcher : MonoBehaviour
+{
+    [SerializeField] private TransformSceneReference playerTransformSceneReference;
+    [SerializeField] private Seeker seeker;
+    [SerializeField] private float radiusOfHighRes = 150f;
+    
+    private NavGraph lowResGraph;
+    private NavGraph highResGraph;
+    private float sqrRadius;
+
+    void Start()
+    {
+        lowResGraph = AstarPath.active.graphs[0];
+        highResGraph = AstarPath.active.graphs[1];
+        sqrRadius = radiusOfHighRes * radiusOfHighRes;
+    }
+
+    void Update()
+    {
+        if (Vector3.SqrMagnitude(transform.position - playerTransformSceneReference.Value.position) < sqrRadius)
+        {
+            seeker.graphMask = GraphMask.FromGraph(highResGraph);
+        }
+        else
+        {
+            seeker.graphMask = GraphMask.FromGraph(lowResGraph);
+        }
+        
+    }
+}
---
+++ Assets/MyAssets/Scripts/BehaviorTreeDesigner/Actions/MoveAStartAgent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using UnityEngine;
+
+public class MoveAStartAgent : MonoBehaviour
+{
+    public SharedTransform Target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

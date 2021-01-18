19dfad327db06ecd308a99390213bb5d605351b4
Dan C <djc5627@gmail.com>
2021-01-18 00:53:54 -0500

    Meat pickup heals hunger

---
+++ Assets/MyAssets/Scripts/Misc/VariableWrappers/IncrementInteger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MyAssets.ScriptableObjects.Variables;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class IncrementInteger : MonoBehaviour
+{
+    [SerializeField] [Required] private IntVariable intVariable;
+
+    public void Increment(int inc)
+    {
+        intVariable.Value += inc;
+    }
+}

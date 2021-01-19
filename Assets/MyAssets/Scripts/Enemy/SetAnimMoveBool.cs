e87365427ccd0809d14fa593513ffb448630ab8d
Dan C <djc5627@gmail.com>
2021-01-18 22:15:33 -0500

    Add forsaken crawler. Fix roll particles mats. Fix state machine missing float. Add EvilWeeds.

---
+++ Assets/MyAssets/Scripts/Enemy/SetAnimMoveBool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetAnimMoveBool : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private CharacterController charController;
+    [SerializeField] private string parameterName = "IsWalking";
+    [SerializeField] private float moveThreshold = 1f;
+
+    private void Update()
+    {
+        if (charController.velocity.sqrMagnitude >= Mathf.Pow(moveThreshold, 2f))
+        {
+            anim.SetBool(parameterName, true);
+        }
+        else
+        {
+            anim.SetBool(parameterName, false);
+        }
+    }
+}

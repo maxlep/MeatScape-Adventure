428ebe08b12bd3642bead22493bc75a0844252da
merc37 <gabeajh@gmail.com>
2021-02-15 00:34:26 -0500

    Add distance from center tracking

---
+++ Assets/MyAssets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,29 @@
+using MyAssets.Scripts.Utils;
+using UnityEngine;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private TransformSceneReference MapCenter;
+    [SerializeField] private TransformSceneReference Player;
+    [SerializeField] private TransformSceneReference DistanceTextTransform;
+
+    public static LevelManager Instance;
+
+    public float DistanceFromCenter {get; private set;}
+
+    private TMP_Text DistanceText;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(this);
+
+        DistanceText = DistanceTextTransform.Value.GetComponent<TMP_Text>();
+    }
+
+    void Update() {
+        DistanceFromCenter = Vector3.Distance(MapCenter.Value.position.xoz(), Player.Value.position.xoz());
+        DistanceText.text = "Dist: " + DistanceFromCenter.ToString();
+    }
+}

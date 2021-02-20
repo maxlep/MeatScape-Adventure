6257bb66793d016fc597340264c38373f2ddff84
Dan C <djc5627@gmail.com>
2021-02-19 22:52:51 -0500

    Only show spawner gizmos when selected

--- Assets/MyAssets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/MyAssets/Scripts/Enemy/EnemySpawner.cs
@@ -66,7 +66,7 @@ public class EnemySpawner : MonoBehaviour
 
     #region Gizmos
     
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
         DrawEnemyMeshGizmo();
     }

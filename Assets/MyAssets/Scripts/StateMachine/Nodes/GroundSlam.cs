e5d66aeaeca1a6a8a6e5926429a87e4e6bd3a9d8
Dan C <djc5627@gmail.com>
2021-02-21 03:33:44 -0500

    Dont override fast speeds with ground slam.

--- Assets/MyAssets/Scripts/StateMachine/Nodes/GroundSlam.cs
+++ Assets/MyAssets/Scripts/StateMachine/Nodes/GroundSlam.cs
@@ -9,15 +9,18 @@ public class GroundSlam : PlayerStateNode
     [HideIf("$collapsed")] [LabelWidth(LABEL_WIDTH)] [SerializeField] private FloatReference SlamVelocity;
     [HideIf("$collapsed")] [LabelWidth(LABEL_WIDTH)] [SerializeField] private FloatReference StoredJumpVelocity;
     [HideIf("$collapsed")] [LabelWidth(LABEL_WIDTH)] [SerializeField] private FloatReference BounceThresholdVelocity;
-
+    [HideIf("$collapsed")] [LabelWidth(LABEL_WIDTH)] [SerializeField] private Vector3Reference PreviousVelocity;
+    
     
     public override void Enter()
     {
         base.Enter();
         
         
-        //Set Velocity at least enough to bounce
-        StoredJumpVelocity.Value = Mathf.Min(-Mathf.Abs(SlamVelocity.Value), -BounceThresholdVelocity.Value);
+        //Set Velocity at least enough to bounce, and dont decrease if already falling fast
+        float newJumpVelocity = Mathf.Min(-Mathf.Abs(SlamVelocity.Value), -BounceThresholdVelocity.Value);
+        newJumpVelocity = Mathf.Min(PreviousVelocity.Value.y, newJumpVelocity);
+        StoredJumpVelocity.Value = newJumpVelocity;
         //playerController.AddImpulse(Vector3.down * SlamForce.Value);
     }
 }

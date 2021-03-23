b9aea90ccadc1aec73d88c23f34cc9c4c6ecb80d
maxlep <mlepkows@stevens.edu>
2021-03-23 14:43:18 -0400

    Add missing scripts.

---
+++ Assets/MyAssets/ScriptableObjects/Variables/ValueReferences/IVector3Value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace MyAssets.ScriptableObjects.Variables.ValueReferences
+{
+    public interface IVector3Value : IValue<Vector3>
+    {
+        Vector3 GetVector3();
+    }
+}
---
+++ Assets/MyAssets/Scripts/MMFeedbacks/MMFeedbackScaleExtended.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MyAssets.ScriptableObjects.Variables;
+using MyAssets.ScriptableObjects.Variables.ValueReferences;
+using MyAssets.Scripts.PoseAnimator.AnimationNodes;
+using UnityEngine;
+
+namespace MoreMountains.Feedbacks
+{
+    [AddComponentMenu("")]
+    [FeedbackPath("Transform/Scale Extended")]
+    [FeedbackHelp("This feedback will animate the target's scale on the 3 specified animation curves, for the specified duration (in seconds). You can apply a multiplier, that will multiply each animation curve value.")]
+    public class MMFeedbackScaleExtended : MMFeedbackScale
+    {
+        // [Header("To Destination")]
+        [MMFEnumCondition("Mode", (int) Modes.ToDestination)]
+        public Vector3Variable DestinationScaleReference;
+
+        private void UpdateDestinationScale()
+        {
+            DestinationScale = DestinationScaleReference.GetVector3();
+        }
+
+        protected override void CustomPlayFeedback(Vector3 position, float attenuation = 1)
+        {
+            if (DestinationScaleReference != null)
+            {
+                UpdateDestinationScale();
+            }
+            
+            base.CustomPlayFeedback(position, attenuation);
+        }
+    }
+}

356ecde3e735c2271a8121005abbcc1f09b41ab3
maxlep <mlepkows@stevens.edu>
2021-03-25 13:23:41 -0400

    Add missing scripts for flavor text.

---
+++ Assets/MyAssets/Scripts/Misc/AssignTextFromFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MyAssets.Scripts.Utils;
+using TMPro;
+using UnityEngine;
+
+namespace MyAssets.Scripts.Misc
+{
+    public class AssignTextFromFile : MonoBehaviour
+    {
+        [SerializeField] private TextAsset _textSource;
+        [SerializeField] private TextMeshProUGUI _textComponent;
+        
+        private void Awake()
+        {
+            if (_textSource == null) return;
+
+            var excerpt = _textSource.TakePassage();
+            _textComponent.text = excerpt;
+        }
+    }
+}
---
+++ Assets/MyAssets/Scripts/Utils/TextAssetUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace MyAssets.Scripts.Utils
+{
+    public static class TextAssetUtils
+    {
+        private static int _subsectionSize = 500;
+        private static string[] _separators = {$"{System.Environment.NewLine}{System.Environment.NewLine}"}; 
+        
+        public static string TakePassage(this TextAsset textAsset)
+        {
+            int randomChar = Random.Range(0, textAsset.text.Length);
+            var subsectionStart = Math.Max(randomChar - _subsectionSize, 0);
+            var subsectionLength = Math.Min(_subsectionSize * 2, textAsset.text.Length - subsectionStart);
+            string subsection = textAsset.text.Substring(subsectionStart, subsectionLength);
+            
+            var split = subsection.Split(_separators, StringSplitOptions.None);
+
+            if (split.Length < 2)
+            {
+                return "ERROR EXTRACTING PASSAGE";
+            }
+            return split[1];
+        }
+    }
+}

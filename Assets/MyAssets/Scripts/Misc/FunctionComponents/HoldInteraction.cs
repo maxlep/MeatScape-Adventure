12c8ce406454b54478f21e35026070c57be58a37
maxlep <mlepkows@stevens.edu>
2021-04-02 22:45:39 -0400

    Add hold interaction and feedback to ruins.

---
+++ Assets/MyAssets/Scripts/Misc/FunctionComponents/HoldInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MyAssets.Scripts.Misc.FunctionComponents
+{
+    public class HoldInteraction : MonoBehaviour
+    {
+        [SerializeField] private TimerReference _timer;
+        [SerializeField] private UnityEvent _action;
+        
+        public void StartTimer()
+        {
+            _timer.RestartTimer();
+        }
+        
+        public void StopTimer()
+        {
+            _timer.StopTimer();
+        }
+        
+        private void Update()
+        {
+            _timer.UpdateTime();
+            if (_timer.IsFinished)
+            {
+                _action.Invoke();
+            }
+        }
+    }
+}

01d81551795a4f0e3de2d97469953acb7312e195
maxlep <mlepkows@stevens.edu>
2021-04-13 01:22:26 -0400

    Remove comments.

--- Assets/MyAssets/Scripts/Player/PlayerHealthController.cs
+++ Assets/MyAssets/Scripts/Player/PlayerHealthController.cs
@@ -18,8 +18,6 @@ public class PlayerHealthController : MonoBehaviour
     [SerializeField] private Gradient healthGradient;
 
     private List<PlayerHealthTickController> healthTickList = new List<PlayerHealthTickController>();
-    private int _displayedHealth = 0;
-    private float tickHeight;
 
     private void Awake()
     {
@@ -47,54 +45,12 @@ public class PlayerHealthController : MonoBehaviour
 
     private void UpdateHealthBar(int prev, int current)
     {
-        var delta = current - _displayedHealth;
-        
         healthTickList.ForEach((t, i) =>
         {
             healthTickList[i].SetFill(i < currentHungerLevel.Value);
         });
-
-        // if (delta > 0)  IncrementHealth(delta);
-        // else if (delta < 0) DecrementHealth(delta);
-
-        _displayedHealth = current;
+        
         UpdateHealthTickColor();
-    }
-
-    private void IncrementHealth(int amount)
-    {
-        // for (int i = 0; i < amount; i++)
-        // {
-        //     GameObject healthTick = Instantiate(healthTickPrefab, healthBarRect);
-        //     currentHealthList.Add(healthTick);
-        // }
-
-        // for (int i = Mathf.Max(currentHungerLevel.Value - amount, 0); i < currentHungerLevel.Value; i++)
-        // {
-        //     healthTickList[i].GetComponent<PlayerHealthTickController>().SetFill(true);
-        // }
-
-        healthTickList.ForEach((t, i) =>
-        {
-            healthTickList[i].SetFill(true);
-        });
-    }
-
-    private void DecrementHealth(int amount)
-    {
-        if (healthTickList.Count < 1) return;
-        
-        // for (int i = 0; i < Mathf.Abs(amount); i++)
-        // {
-        //     int index = currentHealthList.Count - 1;
-        //     Destroy(currentHealthList[index]);
-        //     currentHealthList.RemoveAt(index);
-        // }
-        
-        for (int i = currentHungerLevel.Value; i < Mathf.Min(currentHungerLevel.Value + Mathf.Abs(amount), maxHungerLevel.Value - 1); i++)
-        {
-            healthTickList[i].GetComponent<PlayerHealthTickController>().SetFill(false);
-        }
     }
 
     private void UpdateHealthTickColor()

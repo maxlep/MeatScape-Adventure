22b0ff5fe1c8ed653c9c1b085f297f6f174dbbc9
maxlep <mlepkows@stevens.edu>
2021-05-16 16:58:54 -0400

    Limit shrine to one activation. Delay lightning to strike after stat selected.

--- Assets/MyAssets/Scripts/Misc/FunctionComponents/HoldInteraction.cs
+++ Assets/MyAssets/Scripts/Misc/FunctionComponents/HoldInteraction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,25 +8,44 @@ namespace MyAssets.Scripts.Misc.FunctionComponents
     public class HoldInteraction : MonoBehaviour
     {
         [SerializeField] private TimerReference _timer;
-        [SerializeField] private UnityEvent _action;
-        
-        public void StartTimer()
+        [SerializeField] private UnityEvent _onHoldStart;
+        [SerializeField] private UnityEvent _onHoldCancelled;
+        [SerializeField] private UnityEvent _onHoldFinished;
+        [SerializeField] private int _activationLimit = 0;
+        [ShowInInspector, ReadOnly] private int _activationCount = 0;
+        private bool CanActivate => _activationCount < _activationLimit;
+
+        public void StartHold()
         {
             _timer.RestartTimer();
+            if (CanActivate)
+            {
+                _onHoldStart.Invoke();
+            }
         }
         
-        public void StopTimer()
+        public void StopHold()
         {
             _timer.StopTimer();
+            if (CanActivate)
+            {
+                _onHoldCancelled.Invoke();
+            }
+        }
+
+        public void ResetActivationCount()
+        {
+            _activationCount = 0;
         }
         
         private void Update()
         {
             _timer.UpdateTime();
-            if (_timer.IsFinished)
+            if (_timer.IsFinished && CanActivate)
             {
+                _activationCount++;
                 _timer.ResetTimer();
-                _action.Invoke();
+                _onHoldFinished.Invoke();
             }
         }
     }

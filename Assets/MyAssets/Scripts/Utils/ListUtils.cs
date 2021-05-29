a832a9b655ad4beb4a2076d75e6230f4b7159979
maxlep <mlepkows@stevens.edu>
2021-05-29 10:48:40 -0400

    Remove library reference.

---
+++ Assets/MyAssets/Scripts/Utils/ListUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MyAssets.Scripts.Utils
+{
+    public static class ListUtils
+    {
+        public static T TryGetAtIndex<T>(this List<T> list, int index) where T : class
+        {
+            return list != null && list.Count > index && index >= 0 ? list[index] : default (T);
+        }
+    }
+}

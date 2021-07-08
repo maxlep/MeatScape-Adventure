f75b0840e419035da7f2164e63838eef87e30955
maxlep <mlepkows@stevens.edu>
2021-07-07 20:40:12 -0400

    [annex-sync] Add trophy inventory UI prefabs.

---
+++ Assets/MyAssets/Scripts/Misc/SetIntegerText.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MyAssets.ScriptableObjects.Variables;
+using TMPro;
+using UnityEngine;
+
+namespace MyAssets.Scripts.Misc
+{
+    public class SetIntegerText : MonoBehaviour
+    {
+        [SerializeField] private IntReference IntegerValue;
+        [SerializeField] private TextMeshProUGUI TextMesh;
+
+        private string _initialText;
+        
+        private void Start()
+        {
+            _initialText = TextMesh.text;
+            IntegerValue.Subscribe(UpdateText);
+        }
+
+        private void OnDestroy()
+        {
+            IntegerValue.Unsubscribe(UpdateText);
+        }
+
+        private void UpdateText()
+        {
+            TextMesh.text = _initialText + IntegerValue.Value.ToString();
+        }
+    }
+}

980d99cfabf49814604268ed57f99a9979a1b207
Dan C <djc5627@gmail.com>
2021-08-10 17:29:11 -0400

    Render terrain details using nature renderer and vegetation engine.

--- Assets/MyAssets/Scripts/NatureRenderer/NatureRendererSetter.cs
+++ Assets/MyAssets/Scripts/NatureRenderer/NatureRendererSetter.cs
@@ -8,11 +8,15 @@ public class NatureRendererSetter : MonoBehaviour
 {
     public NatureRenderer NatureRenderer;
     public float detailDistance = 150f;
+    [Range(0f, 1f)] public float reduceDensityInDistance = .5f;
+    public Vector2 ReduceDensityDistance = new Vector2(250f, 1000f);
 
 
     [Button(ButtonSizes.Large)]
     public void ApplySettings()
     {
         NatureRenderer.DetailDistance = detailDistance;
+        NatureRenderer.ReduceDensityAmount = reduceDensityInDistance;
+        NatureRenderer.ReduceDensityDistance = ReduceDensityDistance;
     }
 }

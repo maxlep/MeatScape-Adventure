fcd9dedc2aa27ff7f839133c30d950e90a3f7fda
Dan C <djc5627@gmail.com>
2021-09-09 14:50:44 -0400

    Comment out ParticleSystemParameterizer awake. Add missing float var. Re-pin all tiles to fix transform errors. Adjust meateor strike curves.

--- Assets/MyAssets/Scripts/Effects/ParticleSystemParameterizer.cs
+++ Assets/MyAssets/Scripts/Effects/ParticleSystemParameterizer.cs
@@ -16,46 +16,46 @@ namespace MyAssets.Scripts.Effects
 
         private void Awake()
         {
-            var particleSystemMain = _particleSystem.main;
-
-            if (_startSpeed != null)
-            {
-                particleSystemMain.startSpeed = new ParticleSystem.MinMaxCurve(_startSpeed.Value);
-                _startSpeed.Subscribe(() =>
-                {
-                    var particleSystemMain = _particleSystem.main;
-                    particleSystemMain.startSpeed = new ParticleSystem.MinMaxCurve(_startSpeed.Value);
-                });
-            }
-
-            if (_startLifetimeMultiplier != null)
-            {
-                particleSystemMain.startLifetimeMultiplier = _startLifetimeMultiplier.Value;
-                _startLifetimeMultiplier.Subscribe(() =>
-                {
-                    var particleSystemMain = _particleSystem.main;
-                    particleSystemMain.startLifetimeMultiplier = _startLifetimeMultiplier.Value;
-                });
-            }
-
-            if (_startSizeMultiplier != null)
-            {
-                var origStartSize = particleSystemMain.startSize;
-                particleSystemMain.startSize = new ParticleSystem.MinMaxCurve
-                (
-                    origStartSize.constantMin * _startSizeMultiplier.Value,
-                    origStartSize.constantMax * _startSizeMultiplier.Value
-                );
-                _startSizeMultiplier.Subscribe(() =>
-                {
-                    var particleSystemMain = _particleSystem.main;
-                    particleSystemMain.startSize = new ParticleSystem.MinMaxCurve
-                    (
-                        origStartSize.constantMin * _startSizeMultiplier.Value,
-                        origStartSize.constantMax * _startSizeMultiplier.Value
-                    );
-                });
-            }
+            // var particleSystemMain = _particleSystem.main;
+            //
+            // if (_startSpeed?.Value != null)
+            // {
+            //     particleSystemMain.startSpeed = new ParticleSystem.MinMaxCurve(_startSpeed.Value);
+            //     _startSpeed.Subscribe(() =>
+            //     {
+            //         var particleSystemMain = _particleSystem.main;
+            //         particleSystemMain.startSpeed = new ParticleSystem.MinMaxCurve(_startSpeed.Value);
+            //     });
+            // }
+            //
+            // if (_startLifetimeMultiplier?.Value != null)
+            // {
+            //     particleSystemMain.startLifetimeMultiplier = _startLifetimeMultiplier.Value;
+            //     _startLifetimeMultiplier.Subscribe(() =>
+            //     {
+            //         var particleSystemMain = _particleSystem.main;
+            //         particleSystemMain.startLifetimeMultiplier = _startLifetimeMultiplier.Value;
+            //     });
+            // }
+            //
+            // if (_startSizeMultiplier?.Value != null)
+            // {
+            //     var origStartSize = particleSystemMain.startSize;
+            //     particleSystemMain.startSize = new ParticleSystem.MinMaxCurve
+            //     (
+            //         origStartSize.constantMin * _startSizeMultiplier.Value,
+            //         origStartSize.constantMax * _startSizeMultiplier.Value
+            //     );
+            //     _startSizeMultiplier.Subscribe(() =>
+            //     {
+            //         var particleSystemMain = _particleSystem.main;
+            //         particleSystemMain.startSize = new ParticleSystem.MinMaxCurve
+            //         (
+            //             origStartSize.constantMin * _startSizeMultiplier.Value,
+            //             origStartSize.constantMax * _startSizeMultiplier.Value
+            //         );
+            //     });
+            // }
         }
     }
 }
